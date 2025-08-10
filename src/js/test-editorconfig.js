e620b9f37aa5b3b82177e3a32c3182e5c0147dfd
Franklin-Andres-Rodriguez <negusnet101@gmail.com>
2025-08-10 14:52:49 -0500

    fix:fix vulnerabilities in npm

---
+++ src/js/test-editorconfig.js
@@ -0,0 +1,9 @@
+function testEditorConfig() {
+    // Esta línea debe tener exactamente 4 espacios de indentación
+    const eurUsdRate = 1.0847;
+
+    if (eurUsdRate > 1.08) {
+        // Esta línea debe tener 8 espacios de indentación
+        console.log('EUR is strong against USD');
+    }
+}
